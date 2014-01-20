2be5d0491d674bc8d0b23de486bab254d2d5900c
al223ec <al223ec@student.lnu.se>
2014-01-20 16:04:23 +0100

    IntroApp

---
+++ IntroApp/IntroApp/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="IntroApp.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="op1" runat="server"></asp:TextBox> +
+        <asp:TextBox ID="op2" runat="server"></asp:TextBox>
+        <asp:Button ID="submit" runat="server" Text="ok" OnClick="submit_Click" />
+        <asp:Label ID="result" runat="server" Text="result"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ IntroApp/IntroApp/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IntroApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            result.Text = "sidan laddad"; 
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            int number1, number2; 
+
+            if(!int.TryParse(op1.Text, out number1))
+            {
+                result.Text = "fel fält 1 innehåller inte ett tal";
+                return; 
+            }
+
+            if (!int.TryParse(op2.Text, out number2))
+            {
+                result.Text = "fel fält 2 innehåller inte ett tal";
+                return; 
+            }
+
+            result.Text = (number1 + number2).ToString();
+        }
+    }
+}
